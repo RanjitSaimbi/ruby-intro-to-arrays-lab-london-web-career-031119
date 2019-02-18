b2206ff4c43f8c9e73b337aaa0848ecd0fbd0e8e
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-18 19:49:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,19 @@ end
 def array_with_two_elements
   return [1, 2]
 end 
+
+def first_element(array)
+  return array[0]
+end
+
+def third_elemement(array)
+  return array[2]
+end
+
+def last_element(array)
+  return[-1]
+end
+
+def length_of_array(array)
+  array.length
+end 
